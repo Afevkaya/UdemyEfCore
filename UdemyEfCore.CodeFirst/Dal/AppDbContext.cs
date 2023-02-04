537fa72110b6c1013374f483b3a0d1b5b1de1b5e
Afevkaya <Afevkaya5151@gmail.com>
2023-02-04 17:05:22 +0300

    Code-First(Migrations) / CodeFirst

---
+++ UdemyEfCore.CodeFirst/Dal/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace UdemyEfCore.CodeFirst.Dal;
+
+// Context class
+public class AppDbContext: DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        Initializer.Build();
+        optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+    }
+}
---
+++ UdemyEfCore.CodeFirst/Initializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace UdemyEfCore.CodeFirst;
+
+// Initializer class
+public class Initializer
+{
+    public static IConfigurationRoot Configuration;
+    
+    // Build metod
+    // appsettings.json dosyasındaki gerekli key value tiplerini okumak için. Mesela Db yolunu
+    public static void Build()
+    {
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        Configuration = builder.Build();
+    }
+}
